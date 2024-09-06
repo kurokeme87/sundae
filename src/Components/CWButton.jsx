d37274303020a58c758452dd1cc861086929f822
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-09-06 05:16:43 +0100

    done

--- src/Components/CWButton.jsx
+++ src/Components/CWButton.jsx
@@ -1,11 +1,11 @@
 // import React from 'react'
 
-const CWButton = ({bgcol}) => {
+const CWButton = ({bgcol, text}) => {
   bgcol = bgcol?bgcol:'gradient-to-r from-secondary to-primary hover:from-secondary-300 hover:to-primary-300'
-  
+  text = text?text:"Connect"
   return (
-    <button className={`flex gap-2 items-center justify-center disabled:cursor-not-allowed disabled:opacity-60 group transition duration-300 border active:scale-[0.975] relative inline-flex justify-center items-center overflow-hidden group border-none text-white bg-${bgcol} min-w-[125px] px-5 text-lg h-full max-h-11 rounded-lg lg:w-fit`}>
-        Connect Wallet
+    <button className={`flex gap-2 items-center justify-center disabled:cursor-not-allowed disabled:opacity-60 group transition duration-300 border active:scale-[0.975] relative overflow-hidden group border-none text-white bg-${bgcol} min-w-[125px] px-5 text-lg h-11 rounded-lg lg:w-fit`}>
+        {text}
     </button>
   )
 }
