a1e4f256da35951d73d2858c5459d52cc6b08048
Vachilla64 <68883197+Vachilla64@users.noreply.github.com>
2024-09-20 05:04:29 +0100

    all done

--- src/Components/TableSection.jsx
+++ src/Components/TableSection.jsx
@@ -145,8 +145,9 @@ const TableSection = ({ title, showDelegationInstead }) => {
                                 {
                                     ["Pool", "HRA", `${showDelegationInstead ? "Community Delegation" : "Price"}`, "TVL", "Manage"].map((item, index) => (
                                         <th className='pl-4' key={index}>
-                                            <h4 className={`inline ${item == "Community Delegation" ? "text-gray-200" : ""}`}>
+                                            <h4 className={`flex w-fit gap-1 items-center ${item == "Community Delegation" ? "text-gray-200" : ""}`}>
                                                 {item}
+                                                <div class="inline"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" className="h-2 w-2 opacity-100"><path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"></path></svg><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" class="-mt-[2px] h-2 w-2 opacity-100"><path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"></path></svg></div>
                                             </h4>
                                         </th>
                                     ))
